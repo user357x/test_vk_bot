12792d72160f2adc2332ec76fee3921419181cb8
sergey <user357x@yandex.ru>
2017-04-06 12:38:33 +0700

    e

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
 "use strict"
 
-const request = require('superagent');
+const group_token = 'a7acb2c3d44ad4102eeb7800f01212d2363b52f96c7f8c37c798205f1d43dcac1e9d446a6a5d59358a032';
+
+
 const VK = require("VK-Promise");
+const vk = new VK(group_token);
 
 const getDate = require('./getDate');
-const tryJSON = require('./tryJson');
+ 
+vk.init_longpoll();
 
-const vk = new VK('a7acb2c3d44ad4102eeb7800f01212d2363b52f96c7f8c37c798205f1d43dcac1e9d446a6a5d59358a032');
-
-vk("messages.getLongPollServer", {
-    use_ssl: 1
-}).then(function next(data) {
-    if (!data.server) throw data;
-    vk.on("LongPollRequest", data);
-    return request.get("https://" + data.server + "?act=a_check&key=" + data.key + "&ts=" + data.ts + "&wait=25&mode=2")
-        .then(res => tryJSON(res.text))
-        .then(body => {
-            vk.on("LongPollResponse", body);
-            if (body.error || body.failed){
-                throw body;
-            }else if (body.updates) {
-            	//console.log(JSON.stringify(body));
-            	if(body.updates[0] && body.updates[0][0] === 7) 
-                	body.updates.map(vk.on.bind(this, "update"));
-                data.ts = body.ts;
-            }
-            return next(data);
-        });
-}).catch(function(e) {
-	console.error(e);
-    vk.on("LongPollError",e);
+vk.on("message", (event, msg) => {	
+	if(!msg.out) 
+		msg.send(getDate());
 });
-
-vk.on("message", (event, msg) => msg.send(getDate()));
